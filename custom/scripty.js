f6eb64b714f377eef533f0617ad9e3f859e3db19
olliegithub65 <olliesmithcoding@gmail.com>
2026-04-14 17:49:32 +0100

    Add files via upload

---
+++ custom/scripty.js
@@ -0,0 +1,4 @@
+console.log("hello lads")
+console.log("whats the craic")
+console.log("if you have found this you are offically a hoimy")
+console.log("welcome to the squad")
---
+++ custom/stylth.cs
@@ -0,0 +1,43 @@
+body {
+  background-color: #11aff7;
+  text-align: center;
+}
+
+h1 {
+  font-family: 'Playfair Display', serif;
+  color: lightpink;
+}
+
+h2 {
+  text-align: center;
+  font-family: 'Montserrat', sans-serif;
+  color: #f0b400;
+}
+
+p {
+  text-align: center;
+  font-family: 'Lora', serif;
+  color: white;
+}
+
+/* Normal link */
+a:link {
+  color: pink;
+}
+
+/* Visited link */
+a:visited {
+  color: purple;
+}
+
+/* When hovering */
+a:hover {
+  color: red;
+}
+
+/* When clicked */
+a:active {
+  color: green;
+}
+	
+	
