8937c6c6ef45609bc58d4ea5ba9f6c8745b00fcd
olliegithub65 <olliesmithcoding@gmail.com>
2026-04-27 12:29:15 +0100

    Update scripty.js

--- custom/scripty.js
+++ custom/scripty.js
@@ -14,7 +14,7 @@ console.log("welcome to the squad")
 
 console.log("hello lads")
 console.log("whats the craic")
-console.log("if you have found this you are offically a hoimy")
+console.log("use code olliecool5664 for 80% off")
 console.log("welcome to the squad")
 console.log("welcome to the squad")
 console.log("welcome to the squad")
